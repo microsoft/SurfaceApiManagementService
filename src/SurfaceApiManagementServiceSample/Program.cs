6e50df88f95d0ab97fe4b47ba8af11a419e6e855
Lu Yang <luy@microsoft.com>
2024-03-18 13:58:08 -0700

    Initial sample

---
+++ src/SurfaceApiManagementServiceSample/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Identity.Client;
+using System.Net.Http.Headers;
+
+// Surface API Management Service app id
+const string SurfaceApiManagementServiceAppId = "05c8c62b-18c7-4dff-9f65-2812120f06f0";
+
+// Your tenant and app information
+string tenantId = "<your-tenant-id>";
+string clientId = "<your-app-client-id>";
+string clientSecret = "<your-app-client-secret>";
+string subscriptionKey = "<your-subscription-key>";
+
+// Request token
+var app = ConfidentialClientApplicationBuilder.Create(clientId)
+    .WithClientSecret(clientSecret)
+    .WithAuthority($"https://login.microsoftonline.com/{tenantId}")
+    .Build();
+var authResult = await app.AcquireTokenForClient([$"{SurfaceApiManagementServiceAppId}/.default"]).ExecuteAsync();
+
+// Create request message
+HttpRequestMessage request = new HttpRequestMessage();
+request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", authResult.AccessToken);
+request.Headers.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
+request.Method = HttpMethod.Put;
+request.RequestUri = new Uri("https://appx-sms-int-apim.azure-api.net/api/external/warranty/enrollment");
+
+// Send the request
+using (var client = new HttpClient())
+{
+    var response = await client.SendAsync(request);
+    response.EnsureSuccessStatusCode();
+}
